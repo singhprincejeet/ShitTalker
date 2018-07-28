b27478e8089c7409641562118083f692c1643fb1
jimmy <livingoscilloscope@gmail.com>
2018-07-28 17:58:27 -0400

    added arduino control

---
+++ arduino/arduino/arduino.ino
@@ -0,0 +1,747 @@
+// Simple Motor Shield sketch
+// -----------------------------------
+//
+// By arduino.cc user "Krodal".
+// June 2012
+// Open Source / Public Domain
+//
+// Using Arduino 1.0.1
+//
+// A simple sketch for the motor shield,
+// without using the Adafruit library.
+//
+// The outputs can be used for DC-motors
+// (either full H-bridge or just On and Off), lights,
+// relays, solenoids, etc.
+// But stepper motors can not be used !
+// Servo motors can be used with the default Servo library.
+//
+// A maximum of 4 DC motors can be used with full-bridge,
+// or a maximum of 8 normal outputs, or a combination.
+// Two servo motors can always be used, they use the +5V
+// of the Arduino board, so the voltage regulator could
+// get hot.
+//
+// Tested with an Ebay clone with the Arduino Uno.
+//
+// Parts of the code are from an old Adafruit Motor Shield
+// library, which was public domain at that time.
+// This code is also public domain
+//
+// This simplified program is using the normal
+// Arduino library functions as much as possible.
+//
+// The motors will make a whistling sound,
+// due to the analogWrite() PWM frequency.
+// The Adafruit library is specifically designed to avoid
+// this, so use the Adafruit library for a better result.
+//
+//
+//
+// Connector usage
+// ---------------
+// The order is different than what you would expect.
+// If the Arduino (Uno) board is held with the USB
+// connector to the left, the positive (A) side is
+// at the top (north), and the negative (B) side is
+// the bottom (south) for both headers.
+//
+//   Connector X1:
+//     M1 on outside = MOTOR1_A   (+) north
+//     M1 on inside  = MOTOR1_B   (-)
+//     middle        = GND
+//     M2 on inside  = MOTOR2_A   (+)
+//     M2 on outside = MOTOR2_B   (-) south
+//
+//   Connector X2:
+//     M3 on outside = MOTOR3_B   (-) south
+//     M3 on inside  = MOTOR3_A   (+)
+//     middle        = GND
+//     M4 on inside  = MOTOR4_B   (-)
+//     M4 on outside = MOTOR4_A   (+) north
+//
+//
+//         -------------------------------
+//         | -+s                         |
+//         | -+s                         |
+//    M1 A |                             | M4 A
+//    M1 B |                             | M4 B
+//    GND  |                             | GND
+//    M2 A |                             | M3 A
+//    M2 B |                             | M3 B
+//         |                       ..... |
+//         -------------------------------
+//                + -
+//
+//
+//
+// Pin usage with the Motorshield
+// ---------------------------------------
+// Analog pins: not used at all
+//     A0 ... A5 are still available
+//     They all can also be used as digital pins.
+//     Also I2C (A4=SDA and A5=SCL) can be used.
+//     These pins have a breadboard area on the shield.
+// Digital pins: used: 3,4,5,6,7,8,9,10,11,12
+//     Pin 9 and 10 are only used for the servo motors.
+//     Already in use: 0 (RX) and 1 (TX).
+//     Unused: 2,13
+//     Pin 2 has an soldering hole on the board,
+//           easy to connect a wire.
+//     Pin 13 is also connected to the system led.
+// I2C is possible, but SPI is not possible since
+// those pins are used.
+//
+
+
+#include <Servo.h>
+
+// LCD display
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+
+// Arduino pins for the shift register
+#define MOTORLATCH 12
+#define MOTORCLK 4
+#define MOTORENABLE 7
+#define MOTORDATA 8
+
+// 8-bit bus after the 74HC595 shift register
+// (not Arduino pins)
+// These are used to set the direction of the bridge driver.
+#define MOTOR1_A 2
+#define MOTOR1_B 3
+#define MOTOR2_A 1
+#define MOTOR2_B 4
+#define MOTOR3_A 5
+#define MOTOR3_B 7
+#define MOTOR4_A 0
+#define MOTOR4_B 6
+
+// Arduino pins for the PWM signals.
+#define MOTOR1_PWM 11
+#define MOTOR2_PWM 3
+#define MOTOR3_PWM 6
+#define MOTOR4_PWM 5
+#define SERVO1_PWM 10
+#define SERVO2_PWM 9
+
+// Codes for the motor function.
+#define FORWARD 1
+#define BACKWARD 2
+#define BRAKE 3
+#define RELEASE 4
+
+
+// LCD rows
+#define LCD_MESSAGE 0
+#define LCD_DEBUG 1
+
+// piezo buzzer
+int song = 0;
+const int melodyPin = 2;
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+//Mario main theme melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+//Underworld melody
+int underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
+
+
+// Declare classes for Servo connectors of the MotorShield.
+Servo servo_1;
+Servo servo_2;
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Addr, En, Rw, Rs, d4, d5, d6, d7, backlighpin, polarity
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Simple Motor Shield sketch");
+
+  // Use the default "Servo" library of Arduino.
+  // Attach the pin number to the servo library.
+  // This might also set the servo in the middle position.
+  servo_1.attach(SERVO1_PWM);
+  servo_2.attach(SERVO2_PWM);
+  
+  pinMode(melodyPin, OUTPUT);
+  
+  lcd.begin(16,2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello world!");
+  lcd.setCursor(0, 1);
+  lcd.print("Row number: ");
+  lcd.setCursor(12, 1);
+  lcd.print("2");
+}
+
+
+void loop()
+{
+  // Suppose there are two servo motors connected.
+  // Let them move 180 degrees.
+  /*
+  servo_1.write(0);  
+  delay(1000);
+  servo_1.write(180);
+  delay(2000);
+
+  servo_2.write(0);
+  delay(1000);
+  servo_2.write(180);
+  delay(2000);
+  */
+  
+  if (Serial.available()){
+    String lcd_message = "";
+    while(Serial.available())
+    {
+      lcd_message += (char)Serial.read();
+    } 
+    //lcd.clear();
+    /*
+    lcd.setCursor(0,LCD_MESSAGE);
+    lcd.print("                 ");
+    lcd.setCursor(0,LCD_MESSAGE);
+    lcd.print(lcd_message);
+    */
+    lcd_message.toLowerCase();
+    writeLCDMessage(lcd_message, LCD_MESSAGE);
+    
+    if(lcd_message == "mario")
+    {
+     sing(1); 
+    }
+    else if (lcd_message == "stop")
+    {
+      noTone(melodyPin);
+    }
+    
+  }
+  
+  
+
+  // Suppose there is a relay, or light or solenoid
+  // connected to M3_A and GND.
+  // Note that the 'speed' (the PWM, the intensity)
+  // is for both M3_A and M3_B.
+  // The output is a push-pull output (half bridge),
+  // so it can also be used to drive something low.
+  // The 'speed' (the PWM, the intensity) can be set
+  // to zero, that would make the output disabled
+  // and floating.
+  motor_output(MOTOR3_A, HIGH, 255);
+  delay(200);
+  motor_output(MOTOR3_A, LOW, 255);
+
+
+  // Suppose a DC motor is connected to M1_A(+) and M1_B(-)
+  // Let it run full speed forward and half speed backward.
+  // If 'BRAKE' or 'RELEASE' is used, the 'speed' parameter
+  // is ignored.
+  writeLCDMessage("motor 1 forward", LCD_DEBUG);
+  motor(1, FORWARD, 255);
+  delay(200);
+  // Be friendly to the motor: stop it before reverse.
+  //lcd.clear();
+  writeLCDMessage("motor 1 release", LCD_DEBUG);
+  motor(1, RELEASE, 0);
+  delay(50);
+  //lcd.clear();
+  writeLCDMessage("motor 1 reverse", LCD_DEBUG);
+  motor(1, BACKWARD, 128);
+  delay(200);
+  writeLCDMessage("motor 1 release", LCD_DEBUG);
+  motor(1, RELEASE, 0);
+  
+  
+  writeLCDMessage("motor 2 forward", LCD_DEBUG);
+  motor(2, FORWARD, 255);
+  delay(200);
+  // Be friendly to the motor: stop it before reverse.
+  writeLCDMessage("motor 2 release", LCD_DEBUG);
+  motor(2, RELEASE, 0);
+  delay(50);
+  writeLCDMessage("motor 2 reverse", LCD_DEBUG);
+  motor(2, BACKWARD, 128);
+  delay(200);
+  writeLCDMessage("motor 2 release", LCD_DEBUG);
+  motor(2, RELEASE, 0);
+  
+  //sing(1);
+}
+
+
+// LCD message wrapper
+void writeLCDMessage(String message, int row)
+{
+  lcd.setCursor(0,row);
+  lcd.print("                ");
+  lcd.setCursor(0,row);
+  lcd.print(message); 
+  
+}
+
+
+
+void sing(int s) {
+  // iterate over the notes of the melody:
+  song = s;
+  if (song == 2) {
+    Serial.println(" 'Underworld Theme'");
+    int size = sizeof(underworld_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / underworld_tempo[thisNote];
+
+      buzz(melodyPin, underworld_melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      // stop the tone playing:
+      buzz(melodyPin, 0, noteDuration);
+
+    }
+
+  } else {
+
+    Serial.println(" 'Mario Theme'");
+    int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / tempo[thisNote];
+
+      buzz(melodyPin, melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      // stop the tone playing:
+      buzz(melodyPin, 0, noteDuration);
+
+    }
+  }
+}
+
+void buzz(int targetPin, long frequency, long length) {
+  digitalWrite(13, HIGH);
+  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
+  //// there are two phases to each cycle
+  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of seconds to
+  //// get the total number of cycles to produce
+  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
+    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  }
+  digitalWrite(13, LOW);
+
+}
+
+
+
+// Initializing
+// ------------
+// There is no initialization function.
+//
+// The shiftWrite() has an automatic initializing.
+// The PWM outputs are floating during startup,
+// that's okay for the Motor Shield, it stays off.
+// Using analogWrite() without pinMode() is valid.
+//
+
+
+// ---------------------------------
+// motor
+//
+// Select the motor (1-4), the command,
+// and the speed (0-255).
+// The commands are: FORWARD, BACKWARD, BRAKE, RELEASE.
+//
+void motor(int nMotor, int command, int speed)
+{
+  int motorA, motorB;
+
+  if (nMotor >= 1 && nMotor <= 4)
+  {  
+    switch (nMotor)
+    {
+    case 1:
+      motorA   = MOTOR1_A;
+      motorB   = MOTOR1_B;
+      break;
+    case 2:
+      motorA   = MOTOR2_A;
+      motorB   = MOTOR2_B;
+      break;
+    case 3:
+      motorA   = MOTOR3_A;
+      motorB   = MOTOR3_B;
+      break;
+    case 4:
+      motorA   = MOTOR4_A;
+      motorB   = MOTOR4_B;
+      break;
+    default:
+      break;
+    }
+
+    switch (command)
+    {
+    case FORWARD:
+      motor_output (motorA, HIGH, speed);
+      motor_output (motorB, LOW, -1);     // -1: no PWM set
+      break;
+    case BACKWARD:
+      motor_output (motorA, LOW, speed);
+      motor_output (motorB, HIGH, -1);    // -1: no PWM set
+      break;
+    case BRAKE:
+      // The AdaFruit library didn't implement a brake.
+      // The L293D motor driver ic doesn't have a good
+      // brake anyway.
+      // It uses transistors inside, and not mosfets.
+      // Some use a software break, by using a short
+      // reverse voltage.
+      // This brake will try to brake, by enabling
+      // the output and by pulling both outputs to ground.
+      // But it isn't a good break.
+      motor_output (motorA, LOW, 255); // 255: fully on.
+      motor_output (motorB, LOW, -1);  // -1: no PWM set
+      break;
+    case RELEASE:
+      motor_output (motorA, LOW, 0);  // 0: output floating.
+      motor_output (motorB, LOW, -1); // -1: no PWM set
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+
+// ---------------------------------
+// motor_output
+//
+// The function motor_ouput uses the motor driver to
+// drive normal outputs like lights, relays, solenoids,
+// DC motors (but not in reverse).
+//
+// It is also used as an internal helper function
+// for the motor() function.
+//
+// The high_low variable should be set 'HIGH'
+// to drive lights, etc.
+// It can be set 'LOW', to switch it off,
+// but also a 'speed' of 0 will switch it off.
+//
+// The 'speed' sets the PWM for 0...255, and is for
+// both pins of the motor output.
+//   For example, if motor 3 side 'A' is used to for a
+//   dimmed light at 50% (speed is 128), also the
+//   motor 3 side 'B' output will be dimmed for 50%.
+// Set to 0 for completelty off (high impedance).
+// Set to 255 for fully on.
+// Special settings for the PWM speed:
+//    Set to -1 for not setting the PWM at all.
+//
+void motor_output (int output, int high_low, int speed)
+{
+  int motorPWM;
+
+  switch (output)
+  {
+  case MOTOR1_A:
+  case MOTOR1_B:
+    motorPWM = MOTOR1_PWM;
+    break;
+  case MOTOR2_A:
+  case MOTOR2_B:
+    motorPWM = MOTOR2_PWM;
+    break;
+  case MOTOR3_A:
+  case MOTOR3_B:
+    motorPWM = MOTOR3_PWM;
+    break;
+  case MOTOR4_A:
+  case MOTOR4_B:
+    motorPWM = MOTOR4_PWM;
+    break;
+  default:
+    // Use speed as error flag, -3333 = invalid output.
+    speed = -3333;
+    break;
+  }
+
+  if (speed != -3333)
+  {
+    // Set the direction with the shift register
+    // on the MotorShield, even if the speed = -1.
+    // In that case the direction will be set, but
+    // not the PWM.
+    shiftWrite(output, high_low);
+
+    // set PWM only if it is valid
+    if (speed >= 0 && speed <= 255)    
+    {
+      analogWrite(motorPWM, speed);
+    }
+  }
+}
+
+
+// ---------------------------------
+// shiftWrite
+//
+// The parameters are just like digitalWrite().
+//
+// The output is the pin 0...7 (the pin behind
+// the shift register).
+// The second parameter is HIGH or LOW.
+//
+// There is no initialization function.
+// Initialization is automatically done at the first
+// time it is used.
+//
+void shiftWrite(int output, int high_low)
+{
+  static int latch_copy;
+  static int shift_register_initialized = false;
+
+  // Do the initialization on the fly,
+  // at the first time it is used.
+  if (!shift_register_initialized)
+  {
+    // Set pins for shift register to output
+    pinMode(MOTORLATCH, OUTPUT);
+    pinMode(MOTORENABLE, OUTPUT);
+    pinMode(MOTORDATA, OUTPUT);
+    pinMode(MOTORCLK, OUTPUT);
+
+    // Set pins for shift register to default value (low);
+    digitalWrite(MOTORDATA, LOW);
+    digitalWrite(MOTORLATCH, LOW);
+    digitalWrite(MOTORCLK, LOW);
+    // Enable the shift register, set Enable pin Low.
+    digitalWrite(MOTORENABLE, LOW);
+
+    // start with all outputs (of the shift register) low
+    latch_copy = 0;
+
+    shift_register_initialized = true;
+  }
+
+  // The defines HIGH and LOW are 1 and 0.
+  // So this is valid.
+  bitWrite(latch_copy, output, high_low);
+
+  // Use the default Arduino 'shiftOut()' function to
+  // shift the bits with the MOTORCLK as clock pulse.
+  // The 74HC595 shiftregister wants the MSB first.
+  // After that, generate a latch pulse with MOTORLATCH.
+  shiftOut(MOTORDATA, MOTORCLK, MSBFIRST, latch_copy);
+  delayMicroseconds(5);    // For safety, not really needed.
+  digitalWrite(MOTORLATCH, HIGH);
+  delayMicroseconds(5);    // For safety, not really needed.
+  digitalWrite(MOTORLATCH, LOW);
+}
